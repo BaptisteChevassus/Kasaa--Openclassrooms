67143c1fd7df814ce7cc9baa3e51c6bc27fddd7d
Baptiste Chevassus <baptistechevassuswebdesign@gmail.com>
2026-08-31 19:38:31 +0200

    feat: add 404 error page

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router'
+import './Error404.scss'
+
 function Error404() {
-  return <h1>Error404</h1>
+  return (
+    <div className="error404">
+      <h1 className="error404__code">404</h1>
+      <p className="error404__message">
+        Oups! La page que <br />vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="error404__link">
+        Retourner sur la page d&rsquo;accueil
+      </Link>
+    </div>
+  )
 }
 
 export default Error404
